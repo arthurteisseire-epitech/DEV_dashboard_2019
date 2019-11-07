8d5e382f8fb5d4e15debd233c66b9c91e046bb1e
arthur <arthur.teisseire@epitech.eu>
2019-11-07 13:57:15 +0100

    Update docker-compose and find a simple user in server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const mongojs = require('mongojs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
@@ -7,6 +8,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
 app.get('/api/greeting', (req, res) => {
+    const db = mongojs("127.0.0.1/dashboard_db");
+    db.collection('users').findOne({mail: "toto@toto.fr"}, function (err, doc) {
+        console.log(doc);
+    });
+
     const name = req.query.name || 'World';
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
