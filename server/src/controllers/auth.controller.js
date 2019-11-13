c8acc2330e03d4d5262997950df8e229b7603340
arthur <arthur.teisseire@epitech.eu>
2019-11-13 16:42:59 +0100

    Add update in server

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,29 +1,40 @@
-'use strict'
+'use strict';
 
-const User = require('../models/user.model')
-const jwt = require('jsonwebtoken')
-const config = require('../config')
-const httpStatus = require('http-status')
+const User = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const httpStatus = require('http-status');
 
 exports.register = async (req, res, next) => {
   try {
-    const body = req.body
-    const user = new User(body)
-    const savedUser = await user.save()
-    res.status(httpStatus.CREATED)
+    const body = req.body;
+    const user = new User(body);
+    const savedUser = await user.save();
+    res.status(httpStatus.CREATED);
     res.send(savedUser.transform())
   } catch (error) {
     return next(User.checkDuplicateEmailError(error))
   }
-}
+};
+
+exports.update = async (req, res, next) => {
+  try {
+    const user = await User.findAndGenerateToken(req.body);
+    user.setWeatherCity(req.body.cityname);
+    const savedUser = await user.save();
+    res.send(savedUser.transform())
+  } catch (error) {
+    next(error)
+  }
+};
 
 exports.login = async (req, res, next) => {
   try {
-    const user = await User.findAndGenerateToken(req.body)
-    const payload = {sub: user.id}
-    const token = jwt.sign(payload, config.secret)
+    const user = await User.findAndGenerateToken(req.body);
+    const payload = {sub: user.id};
+    const token = jwt.sign(payload, config.secret);
     return res.json({ message: 'OK', token: token })
   } catch (error) {
     next(error)
   }
-}
+};
--- server/src/routes/auth/auth.route.js
+++ server/src/routes/auth/auth.route.js
@@ -6,9 +6,8 @@ const authController = require('../../controllers/auth.controller');
 const validator = require('express-validation');
 const { create } = require('../../validations/user.validation');
 
-const axios = require('axios');
-
 router.post('/register', validator(create), authController.register);
 router.post('/login', authController.login);
+router.post('/update', authController.update);
 
 module.exports = router;
